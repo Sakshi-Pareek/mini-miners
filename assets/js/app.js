7300421f17c060921ba2fc11f1a619b087d5b528
sakshi pareek <sakshipareek505@gmail.com>
2024-04-03 14:26:18 +0530

    line-height

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,6 @@ function opennav() {
     document.body.classList.toggle('overflow_hidden');
     document.getElementById("menubtn-icon").classList.toggle("cross");
 }
+// ===================================DEFINE-SCRIPT-YEAR ================================ */
+const d = new Date();
+document.getElementById("year_change").innerHTML = d.getFullYear();
